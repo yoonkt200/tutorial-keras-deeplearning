e4b380d80a399b3c9774182a148001721178574d
yoonkt200 <yoonkt200@gmail.com>
2018-08-11 00:57:34 +0900

    structure complete

--- 07-GAN-keras/keras-gan-generate-tutorial.py
+++ 07-GAN-keras/keras-gan-generate-tutorial.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from keras import models
 from keras.layers import Dense, Conv1D, Reshape, Flatten, Lambda
+from keras import optimizers
 from keras.optimizers import Adam
 from keras import backend as K
 
@@ -21,15 +22,48 @@ class Data:
         :param input_dim: input dimension
         """
         self.real_sample = lambda n_batch: np.random.normal(mu, sigma, (n_batch, input_dim))
-        self.in_sample = lambda n_batch: np.random.rand(n_batch, input_dim)
+        self.z_sample = lambda n_batch: np.random.rand(n_batch, input_dim)
 
 
 class GAN:
     """
     Define GAN model
     """
-    def __init__(self):
-        pass
+    def __init__(self, learning_rate, input_dim):
+        self.learning_rate = learning_rate
+        self.input_dim = input_dim
+        self.D = self.discriminator()
+        self.G = self.generator()
+        self.GD = self.makeGD()
+        self.optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999)
+
+    def discriminator(self):
+        D = models.Sequential()
+        D.add(Dense(50, activation='relu', input_shape=(self.input_dim,))) # np.shape(100) == np.shape(100,)과 같은것
+        D.add(Dense(50, activation='relu'))
+        D.add(Dense(1, activation='sigmoid'))
+        D.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        return D
+
+    def generator(self):
+        G = models.Sequential()
+        G.add(Reshape((50, 1), input_shape=(50,)))
+        G.add(Conv1D(50, 1, activation='relu'))
+        G.add(Conv1D(50, 1, activation='sigmoid'))
+        G.add(Conv1D(1, 1))
+        G.add(Flatten())
+        G.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        return G
+
+    def makeGD(self):
+        G, D = self.G, self.D
+        GD = models.Sequential()
+        GD.add(G)
+        GD.add(D)
+        D.trainable = False
+        GD.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        D.trainable = True
+        return GD
 
 
 class Model:
@@ -44,15 +78,62 @@ class Model:
         # the reason why D, G differ in iter : Generator needs more training than Discriminator
         self.n_iter_D = args.n_iter_D
         self.n_iter_G = args.n_iter_G
-        self.model = GAN()
+        self.gan = GAN(self.learning_rate, args.input_dim)
 
     def fit(self):
+        """
+        train model while epoch
+        """
         for epoch in range(self.epochs):
-            pass
+            # train discriminator by real data
+            for iter in range(self.n_iter_D):
+                self.train_D()
+
+            # train GD by generated fake data
+            for iter in range(self.n_iter_G):
+                self.train_GD()
+
+            # print and show each 10n epoch
+            if self._verbose == True and ((epoch + 1) % 10 == 0):
+                gan = self.gan
+                data = self.data
+                z = data.z_sample(100)
+                gen = gan.G.predict(z)
+                real = data.real_sample(100)
+                self.show_hist(real, gen, z)
+                self.print_stat(real, gen)
+
+    def train_D(self):
+        """
+        train Discriminator
+        """
         pass
+
+    def train_GD(self):
+        """
+        train Generator (Not Discriminator)
+        """
+        pass
+
+    def show_hist(self, real, gen, z):
+        """
+        hist on specific train epoch, show generator input z, generated gen, data of real
+        """
+        plt.hist(real.reshape(-1), histtype='step', label='Real')
+        plt.hist(gen.reshape(-1), histtype='step', label='Generated')
+        plt.hist(z.reshape(-1), histtype='step', label='Input')
+        plt.legend(loc=0)
+
+    def print_stats(self, real, gen):
+        """
+        print real data and generated data
+        """
+        print('mu and std of Real:', (np.mean(real), np.std(real)))
+        print('mu and std of Gen:', (np.mean(gen), np.std(gen)))
 
 
 def main():
+    # set hyper parameters
     parser = argparse.ArgumentParser()
     parser.add_argument('--mu', type=float, default=4,
                         help='Mean of population data')
@@ -71,6 +152,8 @@ def main():
     parser.add_argument('--n_iter_G', type=int, default=5,
                         help='training iteration for G')
     args = parser.parse_args()
+
+    # run model
     model = Model(args)
     model.fit()
 
